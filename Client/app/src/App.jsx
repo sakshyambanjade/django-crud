1fa33b13bad92343fcc18b2d1e8de9592d2940c4
Your Name <sakshyambanjade@gmail.com>
2025-04-27 22:34:09 +0545

    added some basics react temp using VITE

---
+++ Client/app/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import React from 'react'
+function App() {
+
+  return (
+    <>
+     <h1> Mero Django Kitab!! </h1>
+     <div>
+      <input type="text" placeholder='Title for the Book'/>
+      <input type="text" placeholder='Placeholder'/>
+      <button>Add you Book</button>
+
+     </div>
+    </>
+  )
+}
+
+export default App
