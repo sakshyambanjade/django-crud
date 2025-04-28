6a068e885a96661a3a46d92cbb76a0d59da8077e
Your Name <sakshyambanjade@gmail.com>
2025-04-28 17:47:26 +0545

    briged the connection between the frontend and the backend

--- Client/app/src/App.jsx
+++ Client/app/src/App.jsx
@@ -1,18 +1,38 @@
-import './App.css'
-import React from 'react'
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/");
+      const data = await response.json();
+      setBooks(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
-     <h1> Mero Django Kitab!! </h1>
-     <div>
-      <input type="text" placeholder='Title for the Book'/>
-      <input type="text" placeholder='Placeholder'/>
-      <button>Add you Book</button>
-
-     </div>
+      <h1>Mero Django Kitab!!</h1>
+      <div>
+        <input type="text" placeholder="Title for the Book" />
+        <input type="text" placeholder="Placeholder" />
+        <button>Add your Book</button>
+      </div>
+      {books.map(((book)=>
+      <div>
+      <p>Title:{book.title}</p>
+      <p>Release Year:{book.release_year}</p>
+      </div>))} 
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Server/server/server/settings.py
+++ Server/server/server/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
     
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'server.urls'
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
